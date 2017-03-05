228665306f687cb0d1ce0507455749a34ccb8d8d
lorong <philippe.lorong@ensam.eu>
2017-03-05 18:22:52 +0000

    first version of a script in order to test all tool scripts

---
+++ sphinxdocs/source/script/Test_all.sh
@@ -0,0 +1,7 @@
+tool_maker.py Test_ball_mill.py
+tool_maker.py Test_cylindrical_mill.py
+tool_maker.py Test_insert_mill_v1v12.py
+tool_maker.py Test_insert_mill.py
+tool_maker.py Test_multi_toolsteps_tool.py
+tool_maker.py Test_sliced_mill.py
+tool_maker.py Test_toroidal_mill.py
